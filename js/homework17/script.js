699bedf34a26d6e14640a80041bd30042115c461
uskrupnik <uskrupnik@gmail.com>
2019-11-13 22:49:45 +0200

    homework17.js

---
+++ js/homework17/script.js
@@ -0,0 +1,53 @@
+'use strict';
+
+
+let x = 60, y = 60 , z = 3;
+
+console.log( (x*y)*z); 
+
+
+
+var num = 1;
+
+num += 12;
+
+num -= 14;
+
+num *= 5;
+
+num /= 7;
+
+num += 1;
+
+num -= 1;
+
+alert(num);
+
+
+
+
+let i = 10, f = 2 , g = 5;
+
+console.log( i + f + g );
+
+
+
+let a = 17, b = 10;
+
+let c = a - b;
+
+let d = 7;
+
+let result = c + d;
+
+console.log( result );
+
+
+
+
+
+let message = 'Юлия';
+
+let phrase = `Привет,${message}!`;
+
+console.log(phrase);
