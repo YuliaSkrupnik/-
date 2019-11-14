e4cb76c9844b9ef7be1db3bdc49974c26b23fe02
uskrupnik <uskrupnik@gmail.com>
2019-11-14 06:50:34 +0200

    homework17.js

--- js/homework17/script.js
+++ js/homework17/script.js
@@ -1,12 +1,12 @@
 'use strict';
 
-
+// №1
 let x = 60, y = 60 , z = 3;
 
 console.log( (x*y)*z); 
 
 
-
+// №2
 var num = 1;
 
 num += 12;
@@ -25,13 +25,13 @@ alert(num);
 
 
 
-
+// №3
 let i = 10, f = 2 , g = 5;
 
 console.log( i + f + g );
 
 
-
+// №4
 let a = 17, b = 10;
 
 let c = a - b;
@@ -44,7 +44,7 @@ console.log( result );
 
 
 
-
+// №5
 
 let message = 'Юлия';
 
