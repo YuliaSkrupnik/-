d5c5869af3777447a0da80dd1be0d634ba0a4b20
uskrupnik <uskrupnik@gmail.com>
2020-03-01 11:32:33 +0200

    js_homework3

---
+++ js_homework3/script.js
@@ -0,0 +1,68 @@
+'uce syrict';
+
+
+let div = document.createElement('div');
+
+div.style.cssText = "background:url(img/0.jpg) no-repeat center center; background-size:cover; width:1280px; height:100vh; position:relative;";
+document.body.append(div);
+
+
+let cat = document.createElement('img');
+cat.setAttribute('src', "img/1.png");
+cat.style.cssText = "position: absolute; width:440px; height:auto; left:430px; top:250px; z-index: 10;";
+div.append(cat);
+
+let img2 = document.createElement('img');
+img2.setAttribute('src', "img/2.png");
+img2.style.cssText = "position: absolute; width:100px; height:auto; right:100px; top:620px;";
+div.append(img2);
+
+let img3 = document.createElement('img');
+img3.setAttribute('src', "img/3.png");
+img3.style.cssText = "position:absolute; width:200px; height:auto; right:650px; top:40px;";
+div.append(img3);
+
+let img4 = document.createElement('img');
+img4.setAttribute('src', "img/4.png");
+img4.style.cssText = "position:absolute; width:200px; height:auto; right:300px; top:270px;";
+div.append(img4);
+
+let img5 = document.createElement('img');
+img5.setAttribute('src', "img/5.png");
+img5.style.cssText = "position:absolute; width:100px; height:auto; right:400px; top:600px;";
+div.append(img5);
+
+let img6 = document.createElement('img');
+img6.setAttribute('src', "img/6.png");
+img6.style.cssText = "position:absolute; width:300px; height:auto; right:100px; top:10px;";
+div.append(img6);
+
+let img7 = document.createElement('img');
+img7.setAttribute('src', "img/7.png");
+img7.style.cssText = "position:absolute; width:100px; height:auto; right:60px; top:550px;";
+div.append(img7);
+
+let img8 = document.createElement('img');
+img8.setAttribute('src', "img/8.png");
+img8.style.cssText = "position:absolute; width:550px; height:auto; left:360px; top:140px;";
+div.append(img8);
+
+let img9 = document.createElement('img');
+img9.setAttribute('src', "img/9.png");
+img9.style.cssText = "position:absolute; width:240px; height:auto; left:100px; top:520px; z-index:9;";
+div.append(img9);
+
+let img10 = document.createElement('img');
+img10.setAttribute('src', "img/10.png");
+img10.style.cssText = "position:absolute; width:100px; height:auto; right:300px; top:650px;";
+div.append(img10);
+
+let img11 = document.createElement('img');
+img11.setAttribute('src', "img/11.png");
+img11.style.cssText = "position:absolute; width:280px; height:auto; left:20px; top:480px;";
+div.append(img11);
+
+
+
+
+
